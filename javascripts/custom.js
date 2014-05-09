fcca81f6a12c8c9b0a4d6a40fb7bdb52b3b0bc4e
Danny Yang <yang_danny19@hotmail.com>
2014-05-08 18:55:13 -0700

    Attempt to reduce lag and various small changes

--- javascripts/custom.js
+++ javascripts/custom.js
@@ -3,26 +3,27 @@ $window = $(window);
 $slide = $('.homeSlide');
 $body = $('body');
 
+$("#slide-1 .bcg").backstretch("images/bcg_slide-1.jpg");
+$("#slide-3 .bcg").backstretch("images/bcg_slide-3.jpg");
+$("#slide-5 .bcg").backstretch("images/bcg_slide-5.png");
+
 //FadeIn all sections   
 $body.imagesLoaded( function() {
 	setTimeout(function() {
-		  
 		// Resize sections
 		adjustWindow();
 		
 		// Always scroll to top
 		$(document).scrollTop(0);
 
-		$("nav").addClass('loaded');
-		
-		// Fade in sections
-		$body.removeClass('loading').addClass('loaded');
-
 		// Set home as active in nav bar
 		$("nav>ul>li.active").removeClass("active");
 		$("#homeA").addClass("active");
 		
-	}, 1000);
+		// Fade in sections
+		$("nav").addClass('loaded');
+		$body.removeClass('loading').addClass('loaded');
+	}, 800);
 });
 
 function adjustWindow(){
@@ -53,12 +54,12 @@ function adjustWindow(){
 		animate: true,
 
 		//The easing function to use.
-		easing: 'outCubic',
+		easing: 'linear',
 
 		//How long the animation should take in ms.
 		duration: function(currentTop, targetTop) {
 			//By default, the duration is hardcoded at 500ms.
-			return 1000;
+			return 500;
 
 			//But you could calculate a value based on the current scroll position (`currentTop`) and the target scroll position (`targetTop`).
 			//return Math.abs(currentTop - targetTop) * 10;
@@ -78,22 +79,9 @@ $( document ).ready(function() {
 		}
 	});
 	
-	$("#slide-1 .bcg").backstretch("images/bcg_slide-1.jpg");
-	$("#slide-3 .bcg").backstretch("images/bcg_slide-3.jpg");
-	$("#slide-5 .bcg").backstretch("images/bcg_slide-5.png");
-	
-	$("#homeA").bind("click", function(){
+	$("nav>ul>li").bind("click", function(){
 		$("nav>ul>li.active").removeClass("active");
-		$("#homeA").addClass("active");
+		$(this).addClass("active");
 	});
 	
-	$("#experienceA").bind("click", function(){
-		$("nav>ul>li.active").removeClass("active");
-		$("#experienceA").addClass("active");
-	});
-	
-	$("#projectsA").bind("click", function(){
-		$("nav>ul>li.active").removeClass("active");
-		$("#projectsA").addClass("active");
-	});
 });
