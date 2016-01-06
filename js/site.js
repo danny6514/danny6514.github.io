6b7864f2d57ace0aa283e062d14b1c3b0e2ab2c6
Danny Yang <yang_danny19@hotmail.com>
2016-01-05 22:18:07 -0800

    Adding Projects page

--- js/site.js
+++ js/site.js
@@ -1,103 +1,64 @@
 // Setup variables
 $window = $(window);
-$slide = $('.homeSlide');
+$slide = $('.home-slide');
 $body = $('body');
 $html = $('html');
 
 $("#home-page .bcg").backstretch("images/home-page-bg.jpg");
 $("#experience-page .bcg").backstretch("images/sap-bg.jpg");
-$("#project-page .bcg").backstretch("images/project-page-bg.png");
+$("#project-page .bcg").backstretch("images/project-bg.jpg");
 
 //FadeIn all sections   
 $body.imagesLoaded( function() {
   // Resize sections
   adjustWindow();
 
-  // Always scroll to top
-  $(document).scrollTop(0);
-
-  // Set home as active in nav bar
-  $("nav>ul>li.active").removeClass("active");
-  $("#homeA").addClass("active");
-
   // Fade in sections
   $("nav").addClass('loaded');
   $body.removeClass('loading').addClass('loaded');
 
-  $("#sapLogo").click(function () {
-    $("#experience-page .bcg").backstretch("images/sap-bg.jpg", {fade: 750});
-    $("#experience-content").html($(".job-description [data-company='" + $(this).data("company") + "']").html());
+  $("#experience-page .company-logo").click(function () {
+    $("#experience-content").html($(".content-description [data-content='" + $(this).data("content") + "']").html());
     $("#experience-content").addClass("has-content");
+    $("#experience-page .bcg").backstretch($(this).data("bg"), {fade: 500});
   });
 
-  $("#capcomLogo").click(function () {
-    $("#experience-page .bcg").backstretch("images/dead-rising-bg.jpg", {fade: 750});
-    $("#experience-content").html($(".job-description [data-company='" + $(this).data("company") + "']").html());
-    $("#experience-content").addClass("has-content");
+  $("#project-page .company-logo").click(function () {
+    $("#project-content").html($(".content-description [data-content='" + $(this).data("content") + "']").html());
+    $("#project-content").addClass("has-content");
+    $("#project-page .bcg").backstretch($(this).data("bg"), {fade: 500});
   });
 
-  $("#blackberryLogo").click(function () {
-    $("#experience-page .bcg").backstretch("images/blackberry-bg.jpg", {fade: 750});
-    $("#experience-content").html($(".job-description [data-company='" + $(this).data("company") + "']").html());
-    $("#experience-content").addClass("has-content");
-  });
-
-  $("#amdLogo").click(function () {
-    $("#experience-page .bcg").backstretch("images/amd-bg.jpg", {fade: 750});
-    $("#experience-content").html($(".job-description [data-company='" + $(this).data("company") + "']").html());
-    $("#experience-content").addClass("has-content");
-  });
-
-  $("#ibmLogo").click(function () {
-    $("#experience-page .bcg").backstretch("images/ibm-bg.png", {fade: 750});
-    $("#experience-content").html($(".job-description [data-company='" + $(this).data("company") + "']").html());
-    $("#experience-content").addClass("has-content");
-  });
-
-  $("#scotiaLogo").click(function () {
-    $("#experience-page .bcg").backstretch("images/scotia-bg.jpg", {fade: 750});
-    $("#experience-content").html($(".job-description [data-company='" + $(this).data("company") + "']").html());
-    $("#experience-content").addClass("has-content");
-  });
-
-  $("#bronteLogo").click(function () {
-    $("#experience-page .bcg").backstretch("images/bronte-bg.jpg", {fade: 750});
-    $("#experience-content").html($(".job-description [data-company='" + $(this).data("company") + "']").html());
-    $("#experience-content").addClass("has-content");
+  var target_id;
+  $(".navbar-nav li a").click(function(ev) {
+    ev.preventDefault();
+    target_id = $(this).attr("href");
+    $("body").animate({
+      scrollTop: $(target_id).offset().top 
+    }, "ease-in-out");
+    return false;
   });
 
   setTimeout(function () {
     $("#intro-container").css("opacity", "1");
-  }, 500);
+  }, 600);
 
   $(window).scroll(_.debounce(function () {
-    $(".company-logo").css("transform", "scale(1.2)");
-    setTimeout(function () {
-      $(".company-logo").css("transform", "");
-    }, 500);
-  }, 200));
+    if ($window.scrollTop() === $("#experience-page").offset().top || $window.scrollTop() === $("#project-page").offset().top) {
+      $(".company-logo").css("transform", "scale(1.2)");
+      setTimeout(function () {
+        $(".company-logo").css("transform", "");
+      }, 500);
+    }
+  }, 250));
 });
 
-function isScrolledIntoView(elem) {
-  var $elem = $(elem);
-  var $window = $(window);
-
-  var docViewTop = $window.scrollTop();
-  var docViewBottom = docViewTop + $window.height();
-
-  var elemTop = $elem.offset().top;
-  var elemBottom = elemTop + $elem.height();
-
-  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-
 function adjustWindow(){
-
   var sectionHeight = $(window).height();
   var slideHeight = $slide.height();
 
   var scrollingScreen = false;
-  $(".homeSlide").mousewheel(function(event, delta) {
+  $slide.mousewheel(function(event, delta) {
     if ( !scrollingScreen ) {
       scrollingScreen = true;
 
@@ -105,7 +66,7 @@ function adjustWindow(){
 
       // Chrome places overflow at body, Firefox places whacks at html...
       // Finds slide headers above/below the current scroll top
-      var candidates = $(".homeSlide").filter(function() {
+      var candidates = $slide.filter(function() {
         if ( delta < 0 )
           return $(this).offset().top > top + 1;
         else
@@ -129,32 +90,4 @@ function adjustWindow(){
     }
     return false;
   });
-
-  var target_id;
-  $(".navbar-nav li a").click(function(ev) {
-    ev.preventDefault();
-    target_id = $(this).attr("href");
-    $("html,body").animate({
-      scrollTop: $(target_id).offset().top 
-    }, "ease-in-out");
-    return false;
-  });
-
 }
-
-$( document ).ready(function() {
-
-  // Always scroll to top
-  $(document).scrollTop(0);
-
-  // $(".company-logo[rel]").overlay({
-  //  closeOnClick: true,
-  //  onBeforeLoad: function () {}
-  // });
-
-$("nav>ul>li").bind("click", function(){
-  $("nav>ul>li.active").removeClass("active");
-  $(this).addClass("active");
-});
-
-});
